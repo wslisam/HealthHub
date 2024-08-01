f50f2c268ec0d2ec5ef6c7ea2ee3b26cc2f21bb0
Sam Li <55217028+wslisam@users.noreply.github.com>
2024-08-01 14:45:09 +0800

    Initial commit: Set up Spring Boot project

---
+++ healthhub/src/main/java/com/wslisam/healthhub/HealthhubApplication.java
@@ -0,0 +1,13 @@
+package com.wslisam.healthhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthhubApplication.class, args);
+	}
+
+}
---
+++ healthhub/src/test/java/com/wslisam/healthhub/HealthhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wslisam.healthhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
