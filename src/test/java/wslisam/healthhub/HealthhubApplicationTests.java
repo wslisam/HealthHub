9254d547f315758808900c95cae0c4c6b2946c3c
Sam Li <55217028+wslisam@users.noreply.github.com>
2024-08-01 14:20:53 +0800

    Initial commit: Set up Spring Boot project and folder structure

---
+++ src/test/java/wslisam/healthhub/HealthhubApplicationTests.java
@@ -0,0 +1,13 @@
+package wslisam.healthhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
